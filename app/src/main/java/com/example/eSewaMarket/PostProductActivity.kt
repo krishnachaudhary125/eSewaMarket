b263ae9e4010487962401dc5ac0b35caeae8d85c
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-08-27 17:44:38 +0545

    fix: fixed edge to edge on post product activity

--- app/src/main/java/com/example/eSewaMarket/PostProductActivity.kt
+++ app/src/main/java/com/example/eSewaMarket/PostProductActivity.kt
@@ -2,6 +2,8 @@ package com.example.eSewaMarket
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.example.eSewaMarket.databinding.ActivityPostProductBinding
 import com.example.eSewaMarket.ui.fragments.PostProductFragment
@@ -15,11 +17,27 @@ class PostProductActivity : AppCompatActivity() {
         binding = ActivityPostProductBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        ViewCompat.setOnApplyWindowInsetsListener(binding.postProductLayout){ view, insets ->
+
+            val top = insets.getInsets(WindowInsetsCompat.Type.statusBars()).top
+
+            view.setPadding(
+                view.paddingLeft,
+                top,
+                view.paddingRight,
+                view.paddingBottom
+            )
+            insets
+        }
+
         loadFragment(PostProductFragment())
 
-        binding.postProductBack.setOnClickListener {
-            onBackPressedDispatcher.onBackPressed()
+        binding.postProductToolbar.backBtn.setOnClickListener {
+            onBackPressedDispatcher
+                .onBackPressed()
         }
+
+        binding.postProductToolbar.toolbarTitle.text = "Post Product"
     }
 
     private fun loadFragment(fragment: Fragment) {
