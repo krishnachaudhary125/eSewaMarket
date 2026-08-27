3f8a3c13e945e407c617463c1b4639cbb3a48c8c
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-08-27 11:28:22 +0545

    feat: used geocoder to reveal the location name

--- app/src/main/java/com/example/eSewaMarket/ui/compose/MapScreen.kt
+++ app/src/main/java/com/example/eSewaMarket/ui/compose/MapScreen.kt
@@ -1,14 +1,25 @@
 package com.example.eSewaMarket.ui.compose
 
+import android.location.Geocoder
+import android.os.Build
+import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.CameraPosition
@@ -16,6 +27,10 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.example.eSewaMarket.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.resume
 
 @Composable
 fun MapScreen(
@@ -29,11 +44,56 @@ fun MapScreen(
         )
     }
 
+    var selectedLocation by remember {
+        mutableStateOf(cameraPositionState.position.target)
+    }
+
+    var selectedAddress by remember {
+        mutableStateOf("Select a location")
+    }
+
+    val context = LocalContext.current
+
     LaunchedEffect(cameraPositionState.isMoving) {
         if (!cameraPositionState.isMoving) {
-            val location = cameraPositionState.position.target
+            selectedLocation = cameraPositionState.position.target
+            onLocationSelected(selectedLocation)
+        }
+    }
 
-            onLocationSelected(location)
+    LaunchedEffect(selectedLocation) {
+        selectedAddress = withContext(Dispatchers.IO) {
+
+            try {
+                val geoCoder = Geocoder(context)
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    suspendCancellableCoroutine { continuation ->
+
+                        geoCoder.getFromLocation(
+                            selectedLocation.latitude,
+                            selectedLocation.longitude,
+                            1
+                        ) { addresses ->
+                            continuation.resume(
+                                addresses.firstOrNull()?.getAddressLine(0)
+                                    ?: "Address not found"
+                            )
+                        }
+                    }
+                } else {
+                    @Suppress("DEPRECATION")
+                    geoCoder.getFromLocation(
+                        selectedLocation.latitude,
+                        selectedLocation.longitude,
+                        1
+                    )?.firstOrNull()?.getAddressLine(0)
+                        ?: "Address not found"
+                }
+            } catch (e: Exception) {
+                Log.e("LOCATION", "Unable to get location", e)
+                "Unable to get Location"
+            }
         }
     }
 
@@ -53,6 +113,15 @@ fun MapScreen(
             modifier = Modifier
                 .align(Alignment.Center)
                 .size(48.dp)
+        )
+
+        Text(
+            text = selectedAddress,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(16.dp)
+                .background(Color.White)
+                .padding(8.dp)
         )
     }
 }
