9bebd2649bd601b604b0c6c2a3254ca056e7547e
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-09-01 10:25:18 +0545

    feat: added location permission request

--- app/src/main/java/com/example/eSewaMarket/NewAddressActivity.kt
+++ app/src/main/java/com/example/eSewaMarket/NewAddressActivity.kt
@@ -1,14 +1,22 @@
 package com.example.eSewaMarket
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.eSewaMarket.databinding.ActivityNewAddressBinding
+import com.example.eSewaMarket.utils.LocationPermissionHandler
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class NewAddressActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewAddressBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationPermissionHandler: LocationPermissionHandler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +38,49 @@ class NewAddressActivity : AppCompatActivity() {
             insets
         }
 
+        locationPermissionHandler = LocationPermissionHandler(this)
+
+        fusedLocationClient =
+            LocationServices.getFusedLocationProviderClient(this)
+
         binding.toolbarNewShippingAddress.toolbarTitle.text = "Add your new address"
         binding.toolbarNewShippingAddress.toolbarIcon.setImageResource(R.drawable.ic_close)
         binding.toolbarNewShippingAddress.toolbarIcon.setBackgroundResource(R.drawable.bg_faq_question)
         binding.toolbarNewShippingAddress.backBtn.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+
+        binding.addLocationIcon.setOnClickListener {
+            locationPermissionHandler.runWithLocationPermission {
+                fetchCurrentLocationAndOpenMap()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun fetchCurrentLocationAndOpenMap() {
+
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location ->
+
+                if (location == null) {
+                    return@addOnSuccessListener
+                }
+
+                val latitude = location.latitude
+                val longitude = location.longitude
+
+                val intent = Intent(this, MapActivity::class.java).apply {
+
+                    putExtra("latitude", latitude)
+                    putExtra("longitude", longitude)
+                }
+
+                startActivity(intent)
+            }
+            .addOnFailureListener { exception ->
+
+                Log.e("LOCATION", "Failed to get location", exception)
+            }
     }
 }
