72a0d6b35d48633354ff3046c65a01451e952c8b
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-09-02 14:50:09 +0545

    feat: added extra address field

--- app/src/main/java/com/example/eSewaMarket/data/models/AddressRequest.kt
+++ app/src/main/java/com/example/eSewaMarket/data/models/AddressRequest.kt
@@ -4,8 +4,11 @@ data class AddressRequest(
 
     val fullName: String,
     val phone: String,
+    val province: String,
+    val district: String,
+    val city: String,
+    val postalCode: String,
     val addressName: String,
-    val formattedAddress: String?,
     val isDefaultAddress: Boolean,
     val isBillingAddress: Boolean,
     val label: String?
