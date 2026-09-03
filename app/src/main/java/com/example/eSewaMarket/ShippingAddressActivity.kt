4076b7350580bff8e8704d5beec2892e7a39cd3a
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-09-03 17:05:12 +0545

    feat: displayed the shipping address in activity

--- app/src/main/java/com/example/eSewaMarket/ShippingAddressActivity.kt
+++ app/src/main/java/com/example/eSewaMarket/ShippingAddressActivity.kt
@@ -3,23 +3,49 @@ package com.example.eSewaMarket
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.getValue
 import androidx.core.view.WindowCompat
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.eSewaMarket.data.repository.UserSessionRepository
 import com.example.eSewaMarket.ui.compose.ShippingAddressScreen
+import com.example.eSewaMarket.ui.factory.ViewModelFactoryProvider
+import com.example.eSewaMarket.ui.viewmodel.AddressViewModel
+import com.example.eSewaMarket.utils.AuthNavigator
+import kotlin.collections.emptyList
+import kotlin.getValue
 
 class ShippingAddressActivity : AppCompatActivity() {
+
+    private val addressViewModel: AddressViewModel by viewModels {
+        ViewModelFactoryProvider.addressFactory(this)
+    }
+    private lateinit var userSessionRepository: UserSessionRepository
+    private lateinit var authNavigator: AuthNavigator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.enableEdgeToEdge(window)
 
+        userSessionRepository = UserSessionRepository(this)
+        authNavigator = AuthNavigator(userSessionRepository)
+        addressViewModel.getAddresses()
+
         setContent {
+
+            val addresses by addressViewModel.addresses
+                .collectAsStateWithLifecycle(
+                    initialValue = emptyList()
+                )
+
             ShippingAddressScreen (
+                shippingAddresses = addresses,
                 onBackClick = {
                     onBackPressedDispatcher
                         .onBackPressed()
                 },
-                noOfAddress = 0,
+                noOfAddress = addresses.size,
                 addAddressNow = {
                     val intent = Intent(this, NewAddressActivity::class.java)
                     startActivity(intent)
--- app/src/main/java/com/example/eSewaMarket/ui/compose/ShippingAddressScreen.kt
+++ app/src/main/java/com/example/eSewaMarket/ui/compose/ShippingAddressScreen.kt
@@ -3,6 +3,7 @@ package com.example.eSewaMarket.ui.compose
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,9 +11,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.sp
 import com.example.eSewaMarket.R
+import com.example.eSewaMarket.data.models.AddressResponse
 
 @Composable
 fun ShippingAddressScreen(
+    shippingAddresses: List<AddressResponse>,
     onBackClick: () -> Unit,
     noOfAddress: Int,
     addAddressNow: () -> Unit
@@ -36,19 +39,36 @@ fun ShippingAddressScreen(
 
     ) { innerPadding ->
 
-        if (noOfAddress == 0) {
+        if (noOfAddress > 0) {
+
+            LazyColumn(
+                modifier = Modifier
+                    .padding(innerPadding),
+            ) {
+
+                items(
+                    items = shippingAddresses,
+                    key = { addresses ->
+                        addresses.id
+                    }
+                ) { addresses ->
+
+                    ShippingAddressCard(
+                        fullName = addresses.fullName,
+                        label = addresses.label.toString(),
+                        addressName = "${addresses.addressName}, ${addresses.district}, ${addresses.city} ${addresses.postalCode}",
+                        province = addresses.province
+                    )
+                }
+            }
+
+        } else {
+
             ShippingAddressEmpty(
                 modifier = Modifier
                     .padding(innerPadding),
                 addAddressNow = addAddressNow
             )
-        } else {
-
-            LazyColumn(
-                modifier = Modifier
-                    .padding(innerPadding)
-            ) { }
         }
-
     }
 }
