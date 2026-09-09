982731741620daa91681c69266635b21df2c95c2
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-09-09 21:45:04 +0545

    feat: created confirmation data class

---
+++ app/src/main/java/com/example/eSewaMarket/ui/compose/checkoutConfirmation/ConfirmationData.kt
@@ -0,0 +1,13 @@
+package com.example.eSewaMarket.ui.compose.checkoutConfirmation
+
+import com.example.eSewaMarket.data.models.ProductResponse
+
+data class ConfirmationData(
+
+    val shippingAddress: String,
+    val paymentOption: String,
+    val totalAmount: Double,
+    val taxAmount: Double,
+    val deliveryCharge: Double,
+    val grandTotal: Double
+)
