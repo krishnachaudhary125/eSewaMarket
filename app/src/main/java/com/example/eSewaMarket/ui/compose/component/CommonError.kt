5874eb016128ee0c19393c4bef0f5786dcba19ae
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-09-09 21:52:29 +0545

    feat: changed the ui of error

--- app/src/main/java/com/example/eSewaMarket/ui/compose/component/CommonError.kt
+++ app/src/main/java/com/example/eSewaMarket/ui/compose/component/CommonError.kt
@@ -7,16 +7,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ErrorOutline
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.eSewaMarket.R
 
 @Composable
@@ -36,7 +41,7 @@ fun CommonError(
             imageVector = Icons.Outlined.ErrorOutline,
             contentDescription = null,
             modifier = Modifier.size(48.dp),
-            tint = colorResource(R.color.text_dark_400)
+            tint = colorResource(R.color.green)
         )
 
         Spacer(modifier = Modifier.height(12.dp))
@@ -57,9 +62,25 @@ fun CommonError(
             Spacer(modifier = Modifier.height(16.dp))
 
             Button(
-                onClick = onRetry
+                onClick = onRetry,
+                shape = RoundedCornerShape(16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = colorResource(id = R.color.green),
+                    contentColor = Color.White
+                ),
+                modifier = Modifier
+                    .padding(
+                        vertical = 8.dp,
+                        horizontal = 16.dp
+                    )
             ) {
-                Text("Retry")
+                Text(
+                    "Retry",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 16.sp,
+                    letterSpacing = 1.sp
+                )
             }
         }
     }
