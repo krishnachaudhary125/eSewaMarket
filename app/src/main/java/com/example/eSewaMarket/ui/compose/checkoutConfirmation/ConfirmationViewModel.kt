2cda9dda9e32b68ca2123953dd6a01a5deca9d34
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-09-09 21:45:18 +0545

    feat: created confirmation view model

---
+++ app/src/main/java/com/example/eSewaMarket/ui/compose/checkoutConfirmation/ConfirmationViewModel.kt
@@ -0,0 +1,57 @@
+package com.example.eSewaMarket.ui.compose.checkoutConfirmation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.eSewaMarket.data.models.ProductResponse
+import com.example.eSewaMarket.utils.minimumLoadingTime
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class ConfirmationViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow<ConfirmationUiState>(
+        ConfirmationUiState.Loading
+    )
+
+    val uiState: StateFlow<ConfirmationUiState> = _uiState.asStateFlow()
+
+    private var products: List<ProductResponse> = emptyList()
+    private var confirmationData: ConfirmationData? = null
+
+    fun loadConfirmation(
+        products: List<ProductResponse>,
+        confirmationData: ConfirmationData
+    ) {
+        this.products = products
+        this.confirmationData = confirmationData
+
+        viewModelScope.launch {
+
+            _uiState.value = ConfirmationUiState.Loading
+
+            minimumLoadingTime {
+                Unit
+            }
+
+            _uiState.value = ConfirmationUiState.Success(
+                checkoutProducts = products,
+                confirmationData = confirmationData
+            )
+        }
+    }
+
+    fun retry() {
+        val data = confirmationData ?: return
+
+        loadConfirmation(
+            products = products,
+            confirmationData = data
+        )
+    }
+
+    fun showError(message: String) {
+        _uiState.value = ConfirmationUiState.Error(message)
+    }
+}
