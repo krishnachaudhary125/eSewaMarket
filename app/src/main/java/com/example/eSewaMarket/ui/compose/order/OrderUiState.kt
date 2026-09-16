daee943abaa159cae0b8d7d1440bdf0eb32c2ff8
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-09-16 14:47:28 +0545

    feat: added list in ui state

--- app/src/main/java/com/example/eSewaMarket/ui/compose/order/OrderUiState.kt
+++ app/src/main/java/com/example/eSewaMarket/ui/compose/order/OrderUiState.kt
@@ -1,14 +1,14 @@
 package com.example.eSewaMarket.ui.compose.order
 
+import com.example.eSewaMarket.data.models.OrderDetailResponse
+
 sealed interface OrderUiState {
 
     data object Loading: OrderUiState
 
-    data object Success: OrderUiState
-
-//    data class Success(
-//        val data: OrderData
-//    ): OrderUiState
+    data class Success(
+        val orders: List<OrderDetailResponse>
+    ): OrderUiState
 
     data class Error(
         val message: String
