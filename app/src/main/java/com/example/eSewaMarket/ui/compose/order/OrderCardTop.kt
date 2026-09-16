8fc3fd4390374a3b6750fe5cf5eb7f4596607100
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-09-16 15:03:06 +0545

    feat: optimized the spacing

--- app/src/main/java/com/example/eSewaMarket/ui/compose/order/OrderCardTop.kt
+++ app/src/main/java/com/example/eSewaMarket/ui/compose/order/OrderCardTop.kt
@@ -35,7 +35,12 @@ fun OrderCardTop(
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(
+                start = 16.dp,
+                top = 16.dp,
+                end = 16.dp,
+                bottom = 8.dp
+            )
     ) {
         Row(
             modifier = Modifier
