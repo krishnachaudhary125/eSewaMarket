a97e1d40c6b6b13cab1b47ce75adf198ea8b14d6
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-09-16 17:42:32 +0545

    feat: created order product detail activity

---
+++ app/src/main/java/com/example/eSewaMarket/OrderProductDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.eSewaMarket
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class OrderProductDetailActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
