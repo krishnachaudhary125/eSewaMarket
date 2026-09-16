0c657b079600a0c2e0aa8b52ec372f947725e83f
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-09-16 14:47:48 +0545

    feat: created function for getting orders

--- app/src/main/java/com/example/eSewaMarket/ui/compose/order/OrderViewModel.kt
+++ app/src/main/java/com/example/eSewaMarket/ui/compose/order/OrderViewModel.kt
@@ -1,16 +1,47 @@
 package com.example.eSewaMarket.ui.compose.order
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.eSewaMarket.data.repository.OrderRepository
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
-class OrderViewModel : ViewModel() {
+class OrderViewModel(
+    private val orderRepository: OrderRepository
+) : ViewModel() {
 
     private val _uiState = MutableStateFlow<OrderUiState>(
-        OrderUiState.Success
+        OrderUiState.Loading
     )
-    val uiState: StateFlow<OrderUiState> = _uiState.asStateFlow()
 
+    val uiState: StateFlow<OrderUiState> = _uiState
 
+    fun getAllOrders() {
+
+        viewModelScope.launch {
+            _uiState.value = OrderUiState.Loading
+
+            try {
+                val orders = orderRepository.getAllOrders()
+
+                _uiState.value = OrderUiState.Success(
+                    orders = orders
+                )
+
+            } catch (e: CancellationException) {
+                throw e
+
+            } catch (e: Exception) {
+                _uiState.value = OrderUiState.Error(
+                    message = e.message ?: "Failed to load orders"
+                )
+            }
+        }
+    }
+
+    fun retry(){
+        getAllOrders()
+    }
 }
