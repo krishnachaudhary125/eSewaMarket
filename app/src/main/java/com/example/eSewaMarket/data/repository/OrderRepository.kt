0bba1ec1a0b6b0e61be451c88652dedcccc2dcd4
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-09-17 09:39:07 +0545

    feat: fetched the order detail data to repository

--- app/src/main/java/com/example/eSewaMarket/data/repository/OrderRepository.kt
+++ app/src/main/java/com/example/eSewaMarket/data/repository/OrderRepository.kt
@@ -56,4 +56,13 @@ class OrderRepository(
             token = token
         )
     }
+
+    suspend fun getOrderDetail(id: Long): List<OrderDetailResponse> {
+        val token = getAuthToken()
+
+        return apiService.getOrderDetail(
+            token = token,
+            id = id
+        )
+    }
 }
